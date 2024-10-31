ccf5135fbec4e728dce953ac427d5dc297851e02
pedrodruviaro <pedrodruviaro@gmail.com>
2024-10-31 09:54:51 -0300

    feat: :heavy_plus_sign: add vuefire

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,22 @@ import router from '@/router'
 import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import { Noir } from '@/assets/presets/Noir'
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from '@/libs/firebase'
 
 const app = createApp(App)
+
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+})
 
 app.use(PrimeVue, {
   theme: {
     preset: Noir,
   },
 })
+
 app.use(router)
 
 app.mount('#app')
