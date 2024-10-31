fe068e10d97cab2e07b22b0d4833c786445b972c
pedrodruviaro <pedrodruviaro@gmail.com>
2024-10-31 07:09:00 -0300

    feat: :heavy_plus_sign: add prime vue and noir preset

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import '@/assets/main.css'
 
 import App from '@/App.vue'
 import router from '@/router'
+import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
+import { Noir } from '@/assets/presets/Noir'
 
 const app = createApp(App)
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Noir,
+  },
+})
 app.use(router)
 
 app.mount('#app')
